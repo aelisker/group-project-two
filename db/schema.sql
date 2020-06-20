9a4421f218d31815102ec91c89f3dccab444d313
Aaron Lisker <aelisker@gmail.com>
2020-06-19 18:51:26 -0700

    ran successful post test to ensure basic server function

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS floating_notes;
+CREATE DATABASE floating_notes;
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+require('dotenv').config();
+
+const path = require('path');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+let cookieVar;
+if(process.env.JAWSDB_URL) {
+  cookieVar = process.env.JAWSDB_COOKIE;
+} else {
+  cookieVar = process.env.DB_COOKIE;
+}
+
+const sess = {
+  secret: cookieVar,
+  cookie: {
+    maxAge: (1000 * 60 * 60)
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
+});
